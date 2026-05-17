5145e9b6ee82b3f86cbd50a828b058ba8bf7ef26
KatherineMayen88 <70446775+KatherineMayen88@users.noreply.github.com>
2026-05-16 21:28:37 -0600

    Connect frontend to AWS backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'http://proyecto2-prograweb-backend-env.eba-z2smuvpp.us-east-2.elasticbeanstalk.com'
 });
 
 api.interceptors.request.use((config) => {

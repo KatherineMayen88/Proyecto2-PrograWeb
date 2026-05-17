7dad8c5797f6b8b447d16cf2eace81d84992af19
jeggs2003 <jegodinezgu@correo.url.edu.gt>
2026-05-16 19:52:56 -0600

    adding middlewares backend

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+
+const protect = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: 'No autorizado' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'secretkey');
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Token inválido' });
+  }
+};
+
+const adminOnly = (req, res, next) => {
+  if (req.user.role !== 'admin') {
+    return res.status(403).json({ message: 'Acceso solo para administradores' });
+  }
+
+  next();
+};
+
+module.exports = {
+  protect,
+  adminOnly
+};
